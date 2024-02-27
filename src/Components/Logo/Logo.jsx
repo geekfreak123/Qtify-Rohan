48946bde6e22dc8531ec8127fb50657b0ad17811
geekfreak123 <sonawanerohan502@gmail.com>
2024-02-27 22:54:41 +0530

    Hero-Section Added

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import logoImage from '../../assets/logo.png'
-import "./Logo.css"
 
 function Logo() {
   return (
-    <img id ="logo" src={logoImage} alt="Qtify Logo" width={65} />
+    <img src={logoImage} alt="Qtify Logo" width={65} />
   )
 }
 
