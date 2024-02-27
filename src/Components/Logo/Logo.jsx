1e38aaddcefc6a57e6d598d8d68402a9efef6747
geekfreak123 <sonawanerohan502@gmail.com>
2024-02-27 17:32:22 +0530

    Changes in Navbar

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import logoImage from '../../assets/logo.png'
+import "./Logo.css"
 
 function Logo() {
   return (
-    <img src={logoImage} alt="Qtify Logo" width={65} />
+    <img id ="logo" src={logoImage} alt="Qtify Logo" width={65} />
   )
 }
 
