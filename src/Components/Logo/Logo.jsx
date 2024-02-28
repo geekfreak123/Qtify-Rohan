69f3c09db25f07a60b29e61d140596344ddb43d1
geekfreak123 <sonawanerohan502@gmail.com>
2024-02-28 10:12:14 +0530

    Margin to the left added

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import logoImage from '../../assets/logo.png'
+import styles from './Logo.module.css'
 
 function Logo() {
   return (
-    <img src={logoImage} alt="Qtify Logo" width={65} />
+    <img src={logoImage} alt="Qtify Logo" width={65} className={styles.Logo}/>
   )
 }
 
